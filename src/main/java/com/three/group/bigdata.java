71573536993c43c12fcab3098984b1f755f58ad6
liangchaowei <liangchaowei0820@163.com>
2020-12-14 18:44:19 +0800

    创建目录

---
+++ src/main/java/com/three/group/bigdata.java
@@ -0,0 +1,13 @@
+package com.three.group;
+
+/**
+ * @auther liangchaowei
+ * @create 2020-12-14 18:28
+ */
+public class bigdata {
+    public static void main(String[] args) {
+        System.out.println("你们好");
+        System.out.println("你们好");
+        System.out.println("你们好");
+    }
+}
